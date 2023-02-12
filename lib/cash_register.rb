4d1d2a824b1eb96b73f929590b4854f1a19e3364
Loise <loise.kimani@student.moringaschool.com>
2023-02-13 02:00:30 +0300

    Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+  attr_reader :total, :discount, :items
+
+  def initialize(discount=nil)
+    @total = 0.0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price * quantity
+    quantity.times { @items << title }
+  end
+
+  def apply_discount
+    if @discount
+      @total *= (100 - @discount) / 100.0
+      puts "Discount applied. New total: $#{@total}"
+    else
+      puts "No discount to apply."
+    end
+  end
+end
 
